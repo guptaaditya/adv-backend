a7ddfe79d7964a145b2bba34a835a62548680c26
Aditya Gupta <guptaaditya24@gmail.com>
2020-05-10 04:39:00 +0530

    file name correction

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const controller = require('../Controller/Admin');
 const authController = require('../Controller/Auth/');
-const validators = require('../validate/Admin');
+const validators = require('../validate/admin');
 
 router.route('/payouts')
     .get(authController.preAuthorization, validators.verifyAdmin, controller.getAMonthsPayouts);
